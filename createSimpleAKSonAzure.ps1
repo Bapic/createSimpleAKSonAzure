d1b00e9430613bf88a33e8e60f46011256f62e24
Bapic <43567337+Bapic@users.noreply.github.com>
2020-07-15 22:57:27 +0530

    updated cmd

--- createSimpleAKSonAzure.ps1
+++ createSimpleAKSonAzure.ps1
@@ -1,7 +1,7 @@
 $name="sampleAKSonAzure01"
 $location="eastus"
 az group create -l eastus -n $name
-az aks create -n $name -g $name
+az aks create -n $name -g $name --node-count 1 --enable-addons monitoring --generate-ssh-keys
 az aks get-credentials -n $name -g $name
 set-content clusterinfo.txt (kubectl config get-contexts)
 add-content clusterinfo.txt (kubectl get svc)
